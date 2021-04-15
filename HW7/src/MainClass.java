6d27ab976492215111f7ad1c1a35a0567f7212fa
Kirik1105 <39922306+Kirik1105@users.noreply.github.com>
2021-04-15 18:08:24 +0300

    Update MainClass.java

--- HW7/src/MainClass.java
+++ HW7/src/MainClass.java
@@ -1,11 +1,11 @@
 public class MainClass {
     public static void main(String[] args) {
-        int catNum = 2;
-        Cat[] cat = new Cat[catNum];
+        
+        Cat[] cat = new Cat[2];
         cat[0] = new Cat("Barsik", 5);
         cat[1] = new Cat("Sauron", 5);
         Plate plate = new Plate(10);
-        for (int x = 0; x < catNum; x++) {
+        for (int x = 0; x < 2; x++) {
             System.out.println("----" + cat[x].getName() + "----");
             plate.info();
             cat[x].eat(plate);
